180f2232eaed930b9eaf79d85ec10357b7063fd5
f-squirrel <dmitry.b.danilov@gmail.com>
2020-09-10 03:33:48 -0700

    Updated the commit lint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,8 +1,35 @@
+//module.exports = {
+//
+//    extends: ['@commitlint/config-conventional'],
+//    rules: {
+//        'footer-leading-blank': [2, 'always'],
+//        'footer-max-line-length': [2, 'always', 120],
+//        'body-max-line-length': [2, 'always', 120],
+//    },
+//}
 module.exports = {
+
     extends: ['@commitlint/config-conventional'],
-    rules: {
-        'footer-leading-blank': [2, 'always'],
-        'footer-max-line-length': [2, 'always', 120],
-        'body-max-line-length': [2, 'always', 120],
-    },
-};
+	rules: {
+		'body-leading-blank': [2, 'always'],
+		'body-max-line-length': [2, 'always', 120],
+		'header-max-length': [2, 'always', 69],
+		'subject-case': [
+			2,
+			'never',
+			['lower-case', 'camel-case', 'kebab-case', 'snake-case', 'pascal-case', 'upper-case'],
+		],
+		'subject-empty': [0, 'never'],
+		'subject-full-stop': [2, 'never', '.'],
+        'footer-leading-blank': [0, 'always'],
+		'footer-max-line-length': [0, 'always', 100],
+		'scope-case': [0, 'always', 'lower-case'],
+		'type-case': [0, 'always', 'lower-case'],
+		'type-empty': [0, 'never'],
+		'type-enum': [
+			0,
+			'always',
+			[],
+		],
+	},
+};;
